ae5260e50c3a414d37d033f681b7122b26d6c421
Marcelo Marques <marcelomarques2007@gmail.com>
2019-12-06 13:23:04 -0300

    CHORE: Root import configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,4 +30,11 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
