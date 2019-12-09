62884a21f9cd85da26b2df339de95933d93d8525
Marcelo Marques <marcelomarques2007@gmail.com>
2019-12-09 15:11:21 -0300

    CHORE: Disable ESlint react/jsx-props-no-spreading

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
+    'react/jsx-props-no-spreading': 'off',
     'react/no-unused-state': 'off',
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
