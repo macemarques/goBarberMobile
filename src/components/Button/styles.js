cf598f01b7cf420164967c6133e217066b71ff51
Marcelo Marques <marcelomarques2007@gmail.com>
2019-12-09 15:13:37 -0300

    CHORE: Edit SignIn Page.

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,4 +1,16 @@
 import styled from 'styled-components/native';
-import { BaseButton } from 'react-native-gesture-handler';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled(BaseButton)``;
+export const Container = styled(RectButton)`
+  height: 46px;
+  background: #3b93ff;
+  border-radius: 4px;
+
+  align-items: center;
+  justify-content: center;
+`;
+export const Text = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import { Image } from 'react-native';
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        icon="call"
-        placeholder="Digite seu nome."
-        style={{ marginTop: 30 }}
-      />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            autoCorrect={false}
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita.</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
