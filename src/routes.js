c2a857e23d830b00803e482defc31c3ecf2c6329
Marcelo Marques <marcelomarques2007@gmail.com>
2019-12-12 16:47:43 -0300

    CHORE: Navigation tabs initialized.

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,9 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
-export default (signedIn = false) =>
+export default (isSigned = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
@@ -13,12 +14,25 @@ export default (signedIn = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255,0.5)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
       },
       {
-        initialRouteName: signedIn ? 'App' : 'Sign',
+        initialRouteName: isSigned ? 'App' : 'Sign',
       }
     )
   );
