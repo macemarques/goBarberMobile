128039906e5712cd33d6a511d781275dfdb6ceaa
macemarques <marcelomarques2007@gmail.com>
2019-12-18 12:09:53 -0300

    CHORE: Finished all lessons modules.

--- src/pages/New/Confirm/styles.js
+++ src/pages/New/Confirm/styles.js
@@ -18,5 +18,16 @@ export const Avatar = styled.Image`
 export const Name = styled.Text`
   margin-top: 10px;
   font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
+export const Time = styled.Text`
+  margin-top: 4px;
+  font-size: 18px;
   color: rgba(255, 255, 255, 0.6);
+`;
+
+export const SubmitButton = styled(Button)`
+  align-self: stretch;
+  margin-top: 20px;
 `;
